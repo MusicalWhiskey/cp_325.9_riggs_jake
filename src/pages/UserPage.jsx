8d1386558ec341d8e8a4693d1b37e83c08fe3286
MusicalWhiskey <124202980+MusicalWhiskey@users.noreply.github.com>
2024-11-25 08:40:16 -0700

    title

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -6,6 +6,7 @@ import "../styles/Pages.css"
 export default function UserPage() {
     return (
         <main>
+            <h1 className="title">TickTockToe</h1>
             <UserForm />
         </main>
     );
