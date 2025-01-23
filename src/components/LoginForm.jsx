8f9c3a1af591d31e3e0266adee98e2d75625ed43
MusicalWhiskey <124202980+MusicalWhiskey@users.noreply.github.com>
2025-01-23 07:25:56 -0700

    userform styling update

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const LoginForm = () => {
+    const [credentials, setCredentials] = useState({
+        identifier: '', // This can be either username or email
+        password: ''
+    });
+
+    const handleChange = (e) => {
+        setCredentials({
+            ...credentials,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await axios.post('/api/login', credentials);
+            console.log('User logged in successfully:', response.data);
+        } catch (error) {
+            console.error('Error logging in:', error);
+        }
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input type="text" name="identifier" placeholder="Username or Email" value={credentials.identifier} onChange={handleChange} required />
+            <input type="password" name="password" placeholder="Password" value={credentials.password} onChange={handleChange} required />
+            <button type="submit">Login</button>
+        </form>
+    );
+};
+
+export default LoginForm;
--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -1,13 +1,16 @@
 import React from "react";
-import UserForm from "../components/UserForm";
+import RegistrationForm from "../components/RegistrationForm";
 import "../styles/Pages.css"
+import LoginForm from "../components/LoginForm";
+
 
 
 export default function UserPage() {
     return (
         <main>
             <h1 className="title">TickTockToe</h1>
-            <UserForm />
+            <RegistrationForm />
+            <LoginForm />
         </main>
     );
 }
