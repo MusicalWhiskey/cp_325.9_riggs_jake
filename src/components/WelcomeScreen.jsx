00459caa65f52e454c378ff71b570d439c8de21a
MusicalWhiskey <124202980+MusicalWhiskey@users.noreply.github.com>
2025-01-28 11:06:05 -0700

    welcome component

---
+++ src/components/WelcomeScreen.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const WelcomeScreen = () => {
+
+    const [credentials, setCredentials] = useState({
+        username: '',
+        password: ''
+    });
+
+    const handleChange = (e) => {
+        setCredentials({
+            ...credentials,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    return (
+        <main>
+            <h2 className="Greeting">Welcome, {credentials.username}!</h2>
+            <input type="text" name="username" placeholder="Username" value={credentials.username} onChange={handleChange} required />
+        </main>
+    );
+};
+
+export default WelcomeScreen;
